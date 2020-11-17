e29c3d57a1a7411f9721273ce34bc4cc51518e1a
Prithvi <prithvishyam454@gmail.com>
2020-11-17 11:41:56 +0530

    prithvi

--- sketch.js
+++ sketch.js
@@ -19,6 +19,8 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
+    platform1 = new Ground(200,100,340,290) 
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -61,4 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    platform1.display();
 }
